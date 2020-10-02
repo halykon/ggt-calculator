9496aaac75cc9cec8c4a95e60be1e504b9a507e0
halykon <sicstar21@gmail.com>
2020-10-02 08:52:33 +0200

    +doc

--- Calc.cs
+++ Calc.cs
@@ -1,15 +1,29 @@
 namespace ggt
 {
+    /// <summary>
+    /// This class exposes to methods for
+    /// calculating the lowest common multiple
+    /// and greatest common factor
+    /// </summary>
     public class Calc
     {
         private readonly int _a, _b;
+        /// <summary>
+        /// Constructor for the Calc class
+        /// </summary>
+        /// <param name="a">The first input number</param>
+        /// <param name="b">The second input number</param>
         public Calc(int a, int b)
         {
             _a = a;
             _b = b;
         }
 
-        public int CalcGgt()
+        /// <summary>
+        /// Calculate the greatest common factor
+        /// </summary>
+        /// <returns>Returns the greatest common factor</returns>
+        public int CalcGcf()
         {
             var a = _a;
             var b = _b;
@@ -22,10 +36,14 @@ namespace ggt
                 b = r;
             }
         }
-
-        public int CalcKgv()
+        
+        /// <summary>
+        /// Calculate the lowest common multiple
+        /// </summary>
+        /// <returns>Returns the lowest common multiple</returns>
+        public int CalcLcm()
         {
-            return _a * _b / CalcGgt();
+            return _a * _b / CalcGcf();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,19 +9,26 @@ namespace ggt
         {
             try
             {
+                // Return if there are too few args
                 if (args.Length < 1){ Console.WriteLine("Malformed input"); return; }
-            
+                
+                // TryParse the input from the CLI
                 var x = int.TryParse(args[0], out var a);
                 var y = int.TryParse(args[1], out var b);
             
+                // Return if the input wasn't parsed successfully
                 if (!x || !y) { Console.WriteLine("Malformed input"); return; }
             
+                // Instantiate new class to calc gcf and lcm
                 var calc = new Calc(a, b);
 
-                Console.WriteLine($"GGT: {calc.CalcGgt()}, KGV: {calc.CalcKgv()}");
+                // calc results and write them to STDOUT
+                Console.WriteLine($"GGT: {calc.CalcGcf()}, KGV: {calc.CalcLcm()}");
             }
             catch (Exception e)
             {
+                // Caught the little bastard!
+                // TO THE CONSOLE WITH HIM!
                 Console.WriteLine($"An error occured: {e}");
             }
         }
